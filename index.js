438daa7ea679b98d6f1a2839bc8b6c68db6fab71
Abyers <anthonyw.byers@gmail.com>
2022-01-30 19:39:54 -0600

    general questions layout/writeToFile + init functions developed

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+    {
+        type   :
+        name   :
+        message:
+        
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        console.log(fileName)
+        console.log(data)
+        if (err) {
+            return console.log(err)
+        } else {
+            console.log('README created! Check README.md to see results!');
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("README.md", generateMarkdown(data));
+        console.log(data);
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,16 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ${data.description}
+  ${data.toc}
+  ${data.installation}
+  ${data.usage}
+  ${data.license}
+  ${data.contributing}
+  ${data.tests}
+  ${data.questions}
 `;
 }
 
